e4ca23d56e7a71045c67d98697a518652fe52bcf
Prabhjot <prabhjottechs@gmail.com>
2022-10-23 12:32:06 +0530

    add random pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
-import type { NextPage } from "next";
-import { trpc } from "../utils/trpc";
+import { getOptionsForVote } from "../utils/getRandomPokemon";
 
-const Home: NextPage = () => {
-  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+export default function Home() {
+  const [first, second] = getOptionsForVote();
 
   return (
     <>
@@ -10,14 +9,11 @@ const Home: NextPage = () => {
         <div className='text-2xl text-center'>Which Pokemon is rounder ?</div>
         <div className='p-2' />
         <div className='border rounded p-8 flex justify-between max-w-2xl items-center'>
-          <div className='w-16 h-16 bg-red-200' />
+          <div className='w-16 h-16 bg-red-200'>{first}</div>
           <div className='p-8'>VS</div>
-          <div className='w-16 h-16 bg-red-200' />
+          <div className='w-16 h-16 bg-red-200'>{second}</div>
         </div>
       </div>
     </>
   );
-};
-
-export default Home;
-
+}
